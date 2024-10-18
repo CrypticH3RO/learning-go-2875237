032a0b100fc328589ed9365d5de830af004573e6
CrypticH3RO <aharoon179@hotmail.com>
2024-10-18 05:11:01 -0400

    fin

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 42
+
+	var p *int // default value is nil
+	var p2 = &anInt
+	fmt.Println("Value of p:", p)
+	fmt.Println("Value of p2:", *p2)
+
+	value1 := 42.13
+	pointer1 := &value1 // pointing at the memory address not the value
+
+	fmt.Println("Value 1: ", *pointer1)
+
+	*pointer1 = *pointer1 / 31
+	fmt.Println("Pointer 1: ", *pointer1)
+	fmt.Println("Value 1: ", value1)
+
 }
