db50c8e534f5bb2a4789f0ab083d1bd147943a65
CrypticH3RO <aharoon179@hotmail.com>
2024-10-19 07:26:39 -0400

    fin

--- practice/main.go
+++ practice/main.go
@@ -5,14 +5,34 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
+	poodle := Dog{"Poodle", 10, "Woof"}
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+
+	poodle.Speak()
+	poodle.Sound = "arf"
+	poodle.Speak()
+
+	poodle.SpeakThreeTimes()
+	// still 3 times because a copy was passed in not
+	poodle.SpeakThreeTimes()
 }
 
 // Dog is a struct
 type Dog struct {
 	Breed  string
 	Weight int
+	Sound  string
+}
+
+// dog method
+// Speak is how hte dog speaks
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
+}
+
+func (d Dog) SpeakThreeTimes() {
+	d.Sound = fmt.Sprintf("%v %v %v", d.Sound, d.Sound, d.Sound)
+	fmt.Println(d.Sound)
 }
